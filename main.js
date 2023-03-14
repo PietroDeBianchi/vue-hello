7799209499ee1728650896d5a36057cc808e463c
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-03-14 17:04:32 +0100

    completed

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ createApp({
   data() {
     return {
       link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png',
-      color: '.mainStyle',
+      color: 'maincolor',
       message: 'We know JS sucks, but with us it becomes wonderful',
       joke: 'Ok not true however it sucks less!'
     }
